ce51775c44d4d08ac3e2a40bce5eba7d63711479
Albano Gujamo Júnior <albanojoaogujamo@gmail.com>
2026-05-14 08:26:00 +0200

    Add Ex1.java with modular greeting functionality

---
+++ Java/modularização;]/Ex1.java
@@ -0,0 +1,11 @@
+package Modularizacao;
+
+public class Ex1 {
+    public static void imprimirSaudacao() {
+        System.out.println("Bem-vindo ao mundo da programação modular!");
+    }
+
+    public static void main(String[] args) {
+        imprimirSaudacao();
+    }
+}
